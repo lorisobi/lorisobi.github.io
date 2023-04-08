18de07fced63e5e08d428bd4ee06903d2e6eaab8
lorisobi <lorisobi@outlook.com>
2023-04-08 14:34:31 +0200

    new terminal

--- js/index.js
+++ js/index.js
@@ -2,10 +2,8 @@ document.getElementById("consoleInput").focus();
 
 function search(ele, content) {
     if(event.key === 'Enter') {
-        alert(ele.value);  
         var paragraph = document.getElementById("consoleLog");
-
-        paragraph.innerHTML += "<br />" + content;      
+        paragraph.innerHTML += content + "<br />";      
     }
 }
 
