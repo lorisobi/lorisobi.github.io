faea4a1c32b8dbb93a1e746c32ac210c17e9fa04
lorisobi <lorisobi@outlook.com>
2023-04-08 14:38:09 +0200

    scroll terminal

--- js/index.js
+++ js/index.js
@@ -3,7 +3,8 @@ document.getElementById("consoleInput").focus();
 function search(ele, content) {
     if(event.key === 'Enter') {
         var paragraph = document.getElementById("consoleLog");
-        paragraph.innerHTML += content + "<br />";      
+        paragraph.innerHTML += content + "<br />";
+        location.href += "#bottom";      
     }
 }
 
