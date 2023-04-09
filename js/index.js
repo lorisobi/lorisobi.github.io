113a111469b4fb314ad341aa0f6a4b4401495f89
lorisobi <lorisobi@outlook.com>
2023-04-09 10:27:45 +0200

    first working commands

--- js/index.js
+++ js/index.js
@@ -3,11 +3,20 @@ document.getElementById("consoleInput").focus();
 function search(ele, content) {
     if(event.key === 'Enter') {
         var paragraph = document.getElementById("consoleLog");
-        paragraph.innerHTML += content + "<br />";
+        paragraph.innerHTML += runProgram(content) + "<br />";
         window.location.href = "#bottom";  
         document.getElementById("consoleInput").innerHTML = "";
         document.getElementById("consoleInput").focus();
 
+    }
+}
+
+function runProgram(command){
+    if (command == "0"){
+        return "Moin";
+    }
+    else{
+        return command;
     }
 }
 
